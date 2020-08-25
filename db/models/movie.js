fc8b193bf02d41d329e6664c4c746b5385da0cfe
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-25 17:08:26 -0600

    Setup validation parameters for database entries

--- db/models/movie.js
+++ db/models/movie.js
@@ -10,15 +10,41 @@ module.exports = (sequelize) => {
         },
         title: {
             type: Sequelize.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Sorry, you need to add a title'
+                },
+                notEmpty: {
+                    msg: 'Sorry, you need to add a title'
+                }
+            }
         },
         runtime: {
             type: Sequelize.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Sorry, but you need to provide a runtime'
+                },
+                min: {
+                    args: 1,
+                    msg: 'Sorry, please provide a runtime longer than 0'
+                }
+            }
         },
         releaseDate: {
             type: Sequelize.DATEONLY,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Sorry, but you need to provide a release date'
+                },
+                isAfter: {
+                    args: '1895-12-27',
+                    msg: 'Sorry, you need to enter a date that is after the creation of VHS'
+                }
+            }
         },
         isAvailableOnVHS: {
             type: Sequelize.BOOLEAN,
